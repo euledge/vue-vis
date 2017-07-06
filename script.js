c218e20588a0a0dafee12b2a7c357b9bfeb784d4
Hitoshi Kuroyanagi <hitoshi.kuroyanagi@gmail.com>
2017-07-06 22:57:49 +0900

    translate epoch to moment

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+
+var dates = [];
+dates[0] = new Date(2017, 6, 1).getTime() / 1000;
+dates[1] = new Date(2017, 6, 2).getTime() / 1000;
+dates[2] = new Date(2017, 6, 3).getTime() / 1000;
+dates[3] = new Date(2017, 6, 4).getTime() / 1000;
+dates[4] = new Date(2017, 6, 5).getTime() / 1000;
+dates[5] = new Date(2017, 6, 6).getTime() / 1000;
+dates[6] = new Date(2017, 6, 7).getTime() / 1000;
+
+console.info(dates);
+
+var moments = [];
+dates.forEach(function(element) {
+	moments.push(moment(element))
+}, this);
+console.info(moments);
+
 var Vue = new Vue({
 	el: '#app',
 	data: {
 		vueState: {
-			data1: [{ id: 1, content: 'item 1', start: '2013-04-20' },
-			{ id: 2, content: 'item 2', start: '2013-04-14' },
-			{ id: 3, content: 'item 3', start: '2013-04-18' },
-			{ id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19' },
-			{ id: 5, content: 'item 5', start: '2013-04-25' },
-			{ id: 6, content: 'item 6', start: '2013-04-27' }],
+			data1: [
+		  { id: 1, content: 'item 1', start: moments[0] },
+			{ id: 2, content: 'item 2', start: moments[1] },
+			{ id: 3, content: 'item 3', start: moments[2] },
+			{ id: 4, content: 'item 4', start: moments[3], end: moments[4] },
+			{ id: 5, content: 'item 5', start: moments[5] },
+			{ id: 6, content: 'item 6', start: moments[6] }],
 
 			data2: [{ id: 1, content: 'item 11', start: '2017-04-20' },
 			{ id: 2, content: 'item 12', start: '2017-04-14' },
